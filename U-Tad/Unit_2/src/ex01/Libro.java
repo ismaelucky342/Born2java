3770dcadecfe8b99ab5234c444245d3aaea0ee3c
Ismael Hernández Clemente <ismaelHC342@gmail.com>
2024-12-28 17:47:02 +0100

    java 28/12/2024 dc

--- U-Tad/Unit_2/src/ex01/Libro.java
+++ U-Tad/Unit_2/src/ex01/Libro.java
@@ -13,10 +13,38 @@ Por último, indicar cuál de los 2 tiene más páginas.*/
 package ex01;
 
 public class Libro {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private int ISBN; 
+	private String autor; 
+	private String titulo; 
+	private int numPaginas;
+	public int getISBN() {
+		return ISBN;
 	}
-
+	public void setISBN(int iSBN) {
+		ISBN = iSBN;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public int getNumPaginas() {
+		return numPaginas;
+	}
+	public void setNumPaginas(int numPaginas) {
+		this.numPaginas = numPaginas;
+	} 
+	
+	@Override
+	public String toString() {
+		return "Libro [ISBN=" + ISBN + ", autor=" + autor + ", titulo=" + titulo + ", numPaginas=" + numPaginas + "]";
+	}
+	
 }
